01e5ebd246a7b4b951810820b38ef4523863b668
Hieu Duong <76527212+duongminhhieu@users.noreply.github.com>
2022-07-06 17:14:37 +0700

    Keep calm and commit

--- Src/app/controllers/NewsController.js
+++ Src/app/controllers/NewsController.js
@@ -1,16 +1,14 @@
-
 class NewsController {
-
     // GET /news
-    index(req, res){
+    index(req, res) {
         res.render('news');
     }
 
     // GET /news/:slug
 
-    show(req, res){
-        res.send('new detail')
+    show(req, res) {
+        res.send('new detail');
     }
 }
 
-module.exports = new NewsController;
+module.exports = new NewsController();
--- Src/index.js
+++ Src/index.js
@@ -1,44 +1,42 @@
-const express = require('express')
+const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 const route = require('./routers');
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true,
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
+// xmlhttprequest, fetch, axios,
 
-// xmlhttprequest, fetch, axios, 
-
-
-    //--------------------------------------------//
+//--------------------------------------------//
 const exphbs = require('express-handlebars');
 const { publicDecrypt } = require('crypto');
-const hbs = exphbs.create({ extname: '.hbs' })
-    // TEMPLATE ENGINE
+const hbs = exphbs.create({ extname: '.hbs' });
+// TEMPLATE ENGINE
 
 app.engine(
-  "hbs",
-  exphbs({
-  // defaultLayout: false,
-  layoutsDir: __dirname + "/resources/views/layout",
-  extname: "hbs",
-}))
+    'hbs',
+    exphbs({
+        // defaultLayout: false,
+        layoutsDir: __dirname + '/resources/views/layout',
+        extname: 'hbs',
+    }),
+);
 
 // app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs');
-app.set("views", path.join(__dirname, 'resources\\views')); // cách mình tìm đến file, hệ điều hành window
-
+app.set('views', path.join(__dirname, 'resources\\views')); // cách mình tìm đến file, hệ điều hành window
 
 // console.log('PATH: ', path.join(__dirname, 'resources/views')) //xem đường dẫn
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -46,14 +44,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 //HTTP logger
 app.use(morgan('combined'));
 
-
 // route init
-route(app)
-
-
-
-
+route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
